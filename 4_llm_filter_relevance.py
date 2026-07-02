2e198d5409b34f98fc06752cc91bfb43a1cfbfbb
Pawelek38 <64952004.com@gmail.com>
2026-07-03 03:02:07 +0400

    Fixed Path name

--- 4_llm_filter_relevance.py
+++ 4_llm_filter_relevance.py
@@ -26,7 +26,7 @@ INPUT_FILENAMES_PREFIX = "jimmyl02_postmortems"
 storage = MinIOStorage()
 storage.client.fget_object(
     bucket_name='raw-data',
-    object_name=f"{INPUT_FILENAMES_PREFIX}.jsonl",
+    object_name=f"{INPUT_FILENAMES_PREFIX}_stage3.jsonl",
     file_path=f"{INPUT_FOLDER_NAME}/{INPUT_FILENAMES_PREFIX}_stage3.jsonl",
 )
 
